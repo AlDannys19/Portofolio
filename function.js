7d024e5a9b998d4c0179acf421ffb2244b922908
Aldannys19 <aldannyrian123@gmail.com>
2024-12-04 21:27:04 +0700

    Final Version Portofolio

--- function.js
+++ function.js
@@ -7,3 +7,34 @@ window.addEventListener('scroll', () =>{
     navbar.classList.remove('scrolled');
   }
 })
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
+  const navItems = document.querySelectorAll("nav a"); // Pilih semua link di dalam navbar
+
+  // Toggle navbar saat burger diklik
+  burger.addEventListener("click", () => {
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+
+  // Tutup navbar saat item menu diklik
+  navItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      navLists.classList.remove("nav-active"); // Hapus class untuk menutup navbar
+      burger.classList.remove("toggle-burger"); // Hapus animasi burger
+    });
+  });
+};
+
+navSlide();
+
+window.onbeforeunload =() => {
+  for (const form of document.getElementsByTagName("form")){
+    form.reset();
+  }
+}
+
+
+
